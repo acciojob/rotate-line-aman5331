519ef66845775d7a4c3bb50231f394f0675a8c11
Amandeep Saxena <75635658+aman5331@users.noreply.github.com>
2023-06-22 09:15:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function rotateLine() {
+      const line = document.getElementById('line');
+      let rotation = 0;
+
+      // Function to update the rotation
+      function updateRotation() {
+        rotation += 1;
+        line.style.transform = `rotate(${rotation}deg)`;
+      }
+
+      // Rotate the line every 10 milliseconds
+      setInterval(updateRotation, 10);
+    }
+
+    // Start rotating the line when the page loads
+    window.onload = rotateLine;
